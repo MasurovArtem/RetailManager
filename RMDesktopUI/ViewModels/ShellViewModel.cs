830d1cd0fdda2d71cc0bcfffb8363f41ee62d739
MasurovArtem <53826825+MasurovArtem@users.noreply.github.com>
2020-04-08 14:06:25 +0300

    Replace container reference in ShellViewModel

--- RMDesktopUI/ViewModels/ShellViewModel.cs
+++ RMDesktopUI/ViewModels/ShellViewModel.cs
@@ -8,19 +8,16 @@ namespace RMDesktopUI.ViewModels
     {
         private SalesViewModel _salesVM;
         
-        private SimpleContainer _container;
         private IEventAggregator _events;
 
-        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM, SimpleContainer container)
+        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM)
         {
             _events = events;
             _salesVM = salesVM;
 
-            _container = container;
-
             _events.Subscribe(this);
             
-            ActivateItem(_container.GetInstance<LoginViewModel>());
+            ActivateItem(IoC.Get<LoginViewModel>());
         }
 
         public void Handle(LogOnEvent message)
